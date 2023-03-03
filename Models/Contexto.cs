bd9b7c035cb8df2631c9a1d48292cf683cd68975
cassiolrm30 <cassiolrm30@gmail.com>
2023-03-03 20:56:31 -0300

    forth commit - problema ao inicializar automaticamente uma entidade

--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -15,25 +15,26 @@ namespace TesteAPI.Models
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(Contexto).Assembly);
 
+            modelBuilder.Entity<Opcao>()
+                        .HasData(
+                           new Opcao { Id = 1, Descricao = "Opção I" },
+                           new Opcao { Id = 2, Descricao = "Opção II" },
+                           new Opcao { Id = 3, Descricao = "Opção III" }
+                        );
+
             modelBuilder.Entity<Teste>().Property(t => t.Valor).HasColumnType("decimal(18,2)");
 
             foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
                 relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
 
-            #region Perfil
-            //if (modelBuilder.Entity<Opcao>().Property(t => t.Id) == 0)
-            if (1 == 1)
-            {
-                modelBuilder.ApplyConfiguration(new OpcaoConfiguration());
-                modelBuilder.Entity<Opcao>(entity => { entity.HasIndex(f => f.Descricao).IsUnique(); });
-                modelBuilder.Entity<Opcao>().HasData
-                (
-                    new Opcao { Id = 1, Descricao = "Opção I"   },
-                    new Opcao { Id = 2, Descricao = "Opção II"  },
-                    new Opcao { Id = 3, Descricao = "Opção III" }
-                );
-            }
-            #endregion
+            /*modelBuilder.ApplyConfiguration(new OpcaoConfiguration());
+            modelBuilder.Entity<Opcao>(entity => { entity.HasIndex(f => f.Descricao).IsUnique(); });
+            modelBuilder.Entity<Opcao>().HasData
+            (
+                new Opcao { Id = 1, Descricao = "Opção I"   },
+                new Opcao { Id = 2, Descricao = "Opção II"  },
+                new Opcao { Id = 3, Descricao = "Opção III" }
+            );*/
 
             modelBuilder.ApplyConfiguration(new OpcaoConfiguration());
 
